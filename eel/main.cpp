b4ba80e76b23efd2670ffbc420b4cba05e21726c
mjbogusz <mjbogusz+github@gmail.com>
2016-01-17 22:22:15 +0100

    Simple (and sample) shaders added; ShaderLoader put to use in main.cpp

--- eel/main.cpp
+++ eel/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 
-#include "Libs\glew\glew.h"
-#include "Libs\freeglut\freeglut.h"
+#include "Libs/glew/glew.h"
+#include "Libs/freeglut/freeglut.h"
+#include "ShaderLoader.h"
+
+GLuint program;
+
+void init() {
+	glEnable(GL_DEPTH_TEST);
+	ShaderLoader loader;
+	program = loader.createProgram("Shaders\\VertexShader.glsl", "Shaders\\FragmentShader.glsl");
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+}
 
 void renderScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glClearColor(1.0, 0.0, 0.0, 1.0);
+
+	glUseProgram(program);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
 	glutSwapBuffers();
 }
@@ -13,21 +26,18 @@ void renderScene() {
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-	glutInitWindowPosition(500, 500);//optional
-	glutInitWindowSize(800, 600); //optional
-	glutCreateWindow("OpenGL First Window");
+	glutInitWindowPosition(500, 500); //optional
+	glutInitWindowSize(800, 800); //optional
+	glutCreateWindow("EEL");
 
 	glewInit();
-	if (glewIsSupported("GL_VERSION_4_5")) {
-		std::cout << "GLEW 4.5\n";
-	} else {
-		std::cout << "GLEW < 4.5\n";
-	}
 
-	glEnable(GL_DEPTH_TEST);
+	init();
 
 	glutDisplayFunc(renderScene);
 	glutMainLoop();
+
+	glDeleteProgram(program);
 
 	return 0;
 }
