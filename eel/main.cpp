15fc27a2adda7c35dc70a8a7e36c7f924f9fde24
mjbogusz <mjbogusz+github@gmail.com>
2016-01-19 03:10:27 +0100

    GL version bumped to 4.3; Vertex shader now does some actual calculations.

--- eel/main.cpp
+++ eel/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, char** argv) {
 	WindowInfo w(std::string("EEL"), 500, 500, 800, 800, true);
-	ContextInfo c(4, 1, true);
+	ContextInfo c(4, 3, true);
 	FramebufferInfo f(true, true, true, true);
 	Initializer::initGLUT(w, c, f, argc, argv);
 
