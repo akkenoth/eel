bf4366821eac00635e9298ac0394747f68dbb4d5
mjbogusz <mjbogusz+github@gmail.com>
2016-01-21 00:56:25 +0100

    main.cpp modified to match latest changes, added test 512x512 texture.

--- eel/main.cpp
+++ eel/main.cpp
@@ -5,12 +5,29 @@
 
 int main(int argc, char** argv) {
 	Engine* engine = new Engine();
-	engine->init();
-	engine->getShaderManager()->createProgram("colorShader", "Shaders\\VertexShader.glsl", "Shaders\\FragmentShader.glsl");
+	if(!engine->init()) {
+		std::cout << "Engine initialization failed\n";
+		return 1;
+	}
+	engine->getShaderManager()->createProgram("colorShader", "Shaders\\VertexTextureShader.glsl", "Shaders\\FragmentTextureShader.glsl");
+	GLuint program = engine->getShaderManager()->getProgram("colorShader");
+	if(program == 0) {
+		std::cout << "Program initialization failed\n";
+		return 2;
+	}
 
 	Cube* cube = new Cube();
-	cube->setProgram(engine->getShaderManager()->getProgram("colorShader"));
+	cube->setProgram(program);
 	cube->create();
+
+	GLuint texture = engine->getTextureLoader()->loadTexture("Resources\\Textures\\test512.bmp", 512, 512);
+	if(texture == 0) {
+		std::cout << "Texture loading failed\n";
+		return 3;
+
+	}
+
+	cube->setTexture("cubeTexture", texture);
 	engine->getModelManager()->setModel("cube", cube);
 
 	engine->run();
