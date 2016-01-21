e0faecbd66a40b27795190743147506871541f01
mjbogusz <mjbogusz+github@gmail.com>
2016-01-21 20:11:25 +0100

    DebugOutput: fixed callback function type (on linux)

--- eel/Engine/DebugOutput.h
+++ eel/Engine/DebugOutput.h
@@ -13,7 +13,7 @@ private:
 public:
 	DebugOutput();
 	~DebugOutput();
-	static void CALLBACK printDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void* data);
+	static void printDebugOutput(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length, const GLchar *msg, const void* userParam);
 };
 
 #endif // !_ENGINE_DEBUGOUTPUT_H
