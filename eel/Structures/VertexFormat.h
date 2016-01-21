ba216acd91e89e1fa3f7f6c635fdfdd68bd20d2a
mjbogusz <mjbogusz+github@gmail.com>
2016-01-21 22:58:05 +0100

    VertexFormat: added normal, added more universal ctors

--- eel/Structures/VertexFormat.h
+++ eel/Structures/VertexFormat.h
@@ -3,9 +3,12 @@
 
 #include "../Libs/glm/glm.hpp"
 
-struct VertexFormat {
+class VertexFormat {
+public:
 	// X, Y, Z
 	glm::vec3 position;
+	// Normal
+	glm::vec3 normal;
 	// RGBA
 	glm::vec4 color;
 	// Texture X, Y
@@ -13,15 +16,27 @@ struct VertexFormat {
 
 	VertexFormat() {}
 
-	VertexFormat(const glm::vec3& pos, const glm::vec4& col) {
-		position = pos;
-		color = col;
-		texture.x = texture.y = 0;
+	VertexFormat(const glm::vec3& pos, const glm::vec4& col = glm::vec4(0.0f), const glm::vec2& tex = glm::vec2(0.0f)) {
+		construct(pos, pos, tex, col);
 	}
 
-	VertexFormat(const glm::vec3& pos, const glm::vec2& tex) {
+	VertexFormat(const glm::vec3& pos, const glm::vec2& tex = glm::vec2(0.0f), const glm::vec4& col = glm::vec4(0.0f)) {
+		construct(pos, pos, tex, col);
+	}
+
+	VertexFormat(const glm::vec3& pos, const glm::vec3& norm, const glm::vec4& col = glm::vec4(0.0f), const glm::vec2& tex = glm::vec2(0.0f)) {
+		construct(pos, norm, tex, col);
+	}
+
+	VertexFormat(const glm::vec3& pos, const glm::vec3& norm, const glm::vec2& tex = glm::vec2(0.0f), const glm::vec4& col = glm::vec4(0.0f)) {
+		construct(pos, norm, tex, col);
+	}
+
+private:
+	void construct(const glm::vec3& pos, const glm::vec3& norm, const glm::vec2& tex, const glm::vec4& col) {
 		position = pos;
-		color.r = color.g = color.b = color.a = 0;
+		normal = glm::normalize(position);
+		color = col;
 		texture = tex;
 	}
 };
