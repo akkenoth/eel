23b56eea157497171b6659054681f5156f7be573
mjbogusz <mjbogusz+github@gmail.com>
2016-01-23 03:13:32 +0100

    TextureLoader: added support for PNG textures through pnglib

--- eel/Engine/TextureLoader.cpp
+++ eel/Engine/TextureLoader.cpp
@@ -3,16 +3,7 @@
 TextureLoader::TextureLoader() {}
 TextureLoader::~TextureLoader() {}
 
-void TextureLoader::loadBMPTexture(const std::string & filename, unsigned int & width, unsigned int & height, unsigned char *& data) {
-	std::ifstream textureFile(filename.c_str(), std::ios::in | std::ios::binary);
-	if(!textureFile.good()) {
-		std::cout << "File not good!\n";
-		width = 0;
-		height = 0;
-		data = NULL;
-		return;
-	}
-
+void TextureLoader::loadBMPTexture(std::ifstream& textureFile, unsigned int & width, unsigned int & height, unsigned char *& data) {
 	BMPHeader header;
 	BMPHeaderInfo headerInfo;
 	header.readFromFile(textureFile);
@@ -39,27 +30,129 @@ void TextureLoader::loadBMPTexture(const std::string & filename, unsigned int & 
 		textureFile.read((char*) rowData, rowWidth);
 		unsigned long p = i * width * 4;
 		for(unsigned int j = 0; j < width; j++) {
-			// Reverse [A]BGR from BMP into RGBA
-			if(byteness == 4) {
-				// If 32bit, read 4 bytes and just reverse order
-				data[p++] = rowData[byteness * j + 3];
-			}
+			// Reverse BGR[A] from BMP into RGBA
 			data[p++] = rowData[byteness * j + 2];
 			data[p++] = rowData[byteness * j + 1];
 			data[p++] = rowData[byteness * j];
-			if(byteness == 3) {
+			if(byteness == 4) {
+				// If 32bit, read 4 bytes and just reverse order
+				data[p++] = rowData[byteness * j + 3];
+			} else {
 				// If 24bit, read 3 bytes and 255 (max) to alpha channel
 				data[p++] = 255;
 			}
 		}
 	}
+}
 
-	textureFile.close();
+void readPNGData(png_structp png_ptr, png_bytep data, png_size_t length) {
+	std::istream* file = (std::istream*) png_get_io_ptr(png_ptr);
+	file->read((char*) data, length);
+}
+
+void TextureLoader::loadPNGTexture(std::ifstream& textureFile, unsigned int& width, unsigned int& height, unsigned char*& data) {
+	// Read and validate header
+	png_byte header[8];
+	textureFile.read((char*) header, 8);
+	if (png_sig_cmp(header, 0, 8)) {
+		return;
+	}
+
+	// Create PNG structs
+	png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png_ptr) {
+		return;
+	}
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr) {
+		png_destroy_read_struct(&png_ptr, (png_infopp) NULL, (png_infopp) NULL);
+		return;
+	}
+	png_infop end_info = png_create_info_struct(png_ptr);
+	if (!end_info) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp) NULL);
+		return;
+	}
+
+	// PNG error handling
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return;
+	}
+
+	// Setup stream read function
+	png_set_read_fn(png_ptr, (png_voidp)& textureFile, readPNGData);
+	
+	// Read PNG info
+	png_set_sig_bytes(png_ptr, 8);
+	png_read_info(png_ptr, info_ptr);
+
+	png_set_strip_16(png_ptr);
+	png_set_expand_gray_1_2_4_to_8(png_ptr);
+	png_set_palette_to_rgb(png_ptr);
+	if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) {
+		png_set_tRNS_to_alpha(png_ptr);
+	}
+	png_read_update_info(png_ptr, info_ptr);
+	
+	int channels = png_get_channels(png_ptr, info_ptr);
+	if(channels != 4) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return;
+	}
+
+	// Allocate memory for data
+	int rowbytes = png_get_rowbytes(png_ptr, info_ptr);
+	data = new unsigned char[rowbytes * height];
+	png_bytep *row_pointers = new png_bytep[height];
+	if (!data || !row_pointers) {
+		png_destroy_read_struct(&png_ptr, &info_ptr, &end_info);
+		return;
+	}
+
+	// Set up row_pointers to point at the correct offsets of data[]
+	for(unsigned int i = 0; i < height; ++i) {
+		//row_pointers[height - 1 - i] = data + i * rowbytes; // same thing
+		row_pointers[i] = data + (height - 1 - i) * rowbytes;
+	}
+
+	// Read the data
+	png_read_image(png_ptr, row_pointers);
 }
 
 GLuint TextureLoader::loadTexture(const std::string & filename, unsigned int width, unsigned int height) {
-	unsigned char* data;
-	loadBMPTexture(filename, width, height, data);
+	unsigned char* data = NULL;
+	if(filename.length() < 5) {
+		return 0;
+	}
+
+	std::string extension = filename.substr(filename.length() - 3, 3);
+	bool isPNG = false;
+	bool isBMP = false;
+	if(extension.compare("png") == 0) {
+		isPNG = true;
+	} else if(extension.compare("bmp") == 0) {
+		isBMP = true;
+	} else {
+		std::cout << "Texture loading error: Bad file extension!\n";
+		return 0;
+	}
+
+	std::ifstream textureFile(filename.c_str(), std::ios::in | std::ios::binary);
+	if(!textureFile.good()) {
+		std::cout << "Texture loading error: File not good!\n";
+		return 0;
+	}
+
+	if(isPNG && !isBMP) {
+		loadPNGTexture(textureFile, width, height, data);
+	} else if(isBMP && !isPNG) {
+		loadBMPTexture(textureFile, width, height, data);
+	} else {
+		return 0;
+	}
+	textureFile.close();
+	
 	if(data == NULL) {
 		return 0;
 	}
--- eel/Engine/TextureLoader.h
+++ eel/Engine/TextureLoader.h
@@ -6,10 +6,12 @@
 #include <string>
 #include "../Libs/glewWrapper.h"
 #include "../Structures/BMPHeader.h"
+#include "../Libs/libpng/png.h"
 
 class TextureLoader {
 private:
-	void loadBMPTexture(const std::string& filename, unsigned int& width, unsigned int& height, unsigned char*& data);
+	void loadBMPTexture(std::ifstream& textureFile, unsigned int& width, unsigned int& height, unsigned char*& data);
+	void loadPNGTexture(std::ifstream& textureFile, unsigned int& width, unsigned int& height, unsigned char*& data);
 public:
 	TextureLoader();
 	~TextureLoader();
