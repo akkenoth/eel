5b63e57a0696e4d07e70e948156fbdd58f74b260
mjbogusz <mjbogusz+github@gmail.com>
2016-01-23 03:20:04 +0100

    FragmentShader: fixed Blinn-Phong implementation, added soft cut-off for specular reflections and implemented energy conserving model, added possibility to change to clean Phong, separated lighting calculation function so that multiple lights can be easily added.

--- eel/Shaders/FragmentShader.glsl
+++ eel/Shaders/FragmentShader.glsl
@@ -1,4 +1,6 @@
 #version 330 core
+// Blinn-Phong shading with attenuation, soft cut-off for specular light and using energy conserving model
+const float PI = 3.14159265;
 
 layout(location = 0) out vec4 out_color; // To framebuffer, color location
 
@@ -13,9 +15,10 @@ uniform vec3 light0Color;
 uniform vec3 light1Position;
 uniform vec3 light1Color;
 
-uniform float materialShininess;
+uniform float materialAmbient;
 uniform float materialDiffusive;
 uniform float materialSpecular;
+uniform float materialShininess;
 
 uniform float attenuationConstant;
 uniform float attenuationLinear;
@@ -27,48 +30,57 @@ in vec3 worldNormal;
 in vec2 textureCoords;
 in vec4 color;
 
+void calculateLight(in vec3 lightPosition, in vec3 lightColor, inout vec3 diffuseSum, inout vec3 specularSum, in bool blinn) {
+	// Direction TO viewer and light
+	vec3 viewDirection = normalize(eyePosition - worldPosition);
+	vec3 lightDirection = normalize(lightPosition - worldPosition);
+	float lambertian = dot(worldNormal, lightDirection);
+	// We're interested in this light source only if angle <= +-90deg
+	float d = distance(lightPosition, worldPosition);
+	float attenuation = 1.0 / (attenuationConstant + d * attenuationLinear + d * d * attenuationQuadratic);
+		
+	if(lambertian > 0.0) {
+		diffuseSum += attenuation * lightColor * materialDiffusive * lambertian;
+	}
+
+	vec3 reflectDirection;
+	float energyCoefficient;
+	if(blinn) {
+		// Blinn-Phong
+		energyCoefficient = (8.0 + materialShininess) / (8.0 * PI);
+		// halfway between direction to light and to viewer
+		reflectDirection = normalize(lightDirection + viewDirection);
+	} else {
+		// Phong
+		energyCoefficient = (2.0 + materialShininess) / (2.0 * PI);
+		// Straight reflection
+		reflectDirection = reflect(-lightDirection, worldNormal);
+	}
+	float angle = dot(worldNormal, reflectDirection);
+	if(angle > 0.0) {
+		specularSum += attenuation * lightColor * materialSpecular * energyCoefficient * pow(angle, materialShininess * 4.0);
+	}
+}
+
 void main(void) {
 	// Base color and texture
 	float timeOffset = timePassed * 0.1;
 	vec4 color0 = texture(texture0, vec2(textureCoords.x, textureCoords.y));
 	out_color = mix(color, color0, color0.a);
 	vec4 color1 = texture(texture1, vec2(textureCoords.x, textureCoords.y - timeOffset));
-	out_color = mix(out_color, color1, color1.a);
+	float alpha = color1.a;
+	color1.a = 1.0;
+	out_color = mix(out_color, color1, alpha);
 
 	// Lights
-	vec3 view = normalize(eyePosition - worldPosition);
+	bool blinn = true;
+	vec3 diffuseSum = vec3(0.0);
+	vec3 specularSum = vec3(0.0);
 
-	vec3 light0 = normalize(light0Position - worldPosition);
-	float distance0 = distance(light0Position, worldPosition);
-	float attenuation0 = 1.0 / (attenuationConstant + distance0 * attenuationLinear + distance0 * distance0 * attenuationQuadratic);
-	float diffuse0 = 0;
-	float specular0 = 0;
-	float lambert0 = dot(light0, worldNormal);
-	if(lambert0 > 0.0) {
-		diffuse0 = lambert0 * materialDiffusive;
-		vec3 halfway = normalize(light0Position + worldPosition);
-		specular0 = materialSpecular * pow(max(0, dot(halfway, worldNormal)), materialShininess);
-	}
-	float light0value = attenuation0 * (diffuse0 + specular0);
-	vec3 light0out = light0Color * light0value;
+	calculateLight(light0Position, light0Color, diffuseSum, specularSum, blinn);
+	calculateLight(light1Position, light1Color, diffuseSum, specularSum, blinn);
 
-	vec3 light1 = normalize(light1Position - worldPosition);
-	float distance1 = distance(light1Position, worldPosition);
-	float attenuation1 = 1.0 / (attenuationConstant + distance1 * attenuationLinear + distance1 * distance1 * attenuationQuadratic);
-	float diffuse1 = 0;
-	float specular1 = 0;
-	float lambert1 = dot(light0, worldNormal);
-	if(lambert1 > 0.0) {
-		diffuse1 = lambert1 * materialDiffusive;
-		vec3 halfway = normalize(light1Position + worldPosition);
-		specular1 = materialSpecular * pow(max(0, dot(halfway, worldNormal)), materialShininess);
-	}
-	float light1value = attenuation1 * (diffuse1 + specular1);
-	vec3 light1out = light1Color * light1value;
-
-	vec3 light = light0 + light1;
-
-	// light?
-	// light = vec3(1.0);
-	out_color = vec4(out_color.r * light.r, out_color.g * light.g, out_color.b * light.b, out_color.a);
+	//Fix alpha
+	// max for nice shiny specular reflections
+	out_color.rgb = out_color.rgb * (diffuseSum + materialAmbient) + max(out_color.rgb, 0.05) * specularSum;
 }
