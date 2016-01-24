e9929bec44009013c5001ff0fae9a7b4f8666a1b
mjbogusz <mjbogusz+github@gmail.com>
2016-01-25 00:06:58 +0100

    main.cpp: modified to match latest code changes - per-scene lights and shader program, setting model's rotation and materials.

--- eel/main.cpp
+++ eel/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 #include "Engine/Engine.h"
-#include "Models/Cube.h"
 #include "Models/Sphere.h"
+#include "Libs/glm/detail/type_vec3.hpp"
 
 #ifdef _WIN32
 #define DIR_SEP '\\'
@@ -16,21 +16,30 @@ int main(int argc, char** argv) {
 		std::cout << "Engine initialization failed\n";
 		return 1;
 	}
-	engine->getShaderManager()->createProgram("colorShader", "Shaders/VertexShader.glsl", "Shaders/FragmentShader.glsl");
-	
-	GLuint program = engine->getShaderManager()->getProgram("colorShader");
+
+	engine->getShaderManager()->createProgram("mainShader", "Shaders/VertexShader.glsl", "Shaders/FragmentShader.glsl");
+	GLuint program = engine->getShaderManager()->getProgram("mainShader");
 	if(program == 0) {
 		std::cout << "Program initialization failed\n";
 		return 2;
 	}
+	engine->getSceneManager()->setProgram(program);
+
+	//"attenuationConstant", 1.0f;
+	//"attenuationLinear", 0.3f;
+	//"attenuationQuadratic", 0.1f;
+	engine->getSceneManager()->addLight(0, glm::vec3(0.0f, 5.0f, 0.0f));
+	engine->getSceneManager()->addLight(0, glm::vec3(3.0f, -5.0f, 0.0f), glm::vec3(4.0f, 1.0f, 1.0f));
 
 	Sphere* sphere = new Sphere();
-	sphere->setProgram(program);
 	sphere->create(1.0, 24, 48);
-	sphere->setTexture("sphereTexture0", engine->getTextureLoader()->loadTexture("Resources/Textures/sphere1.bmp", 256, 256));
-	sphere->setTexture("sphereTexture1", engine->getTextureLoader()->loadTexture("Resources/Textures/sphere_dots.png", 256, 256));
-	engine->getModelManager()->setModel("sphere", sphere);
+	sphere->setRotation(glm::vec3(30.0f, 0.0f, 0.0f));
+	sphere->setRotationSpeed(glm::vec3(0.0f, 20.0f, 0.0f));
+	sphere->addMaterial(0, "Resources/Textures/sphere1.bmp", 0.02f, 0.5f, 1.9f, 10.9f, engine->getTextureLoader());
+	sphere->addMaterial(1, "Resources/Textures/sphere_dots.png", 0.02f, 0.5f, 1.9f, 10.9f, engine->getTextureLoader());
 
+	engine->getModelManager()->setModel("sphere", sphere);
+	
 	engine->run();
 
 	delete engine;
