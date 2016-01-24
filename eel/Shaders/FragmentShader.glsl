efb9b2aa9e73aea70361f68eccf2b726b4a28e79
mjbogusz <mjbogusz+github@gmail.com>
2016-01-25 00:06:02 +0100

    FragmentShader: - rewritten to accept N materials and M light sources (for now N=M=4) - added material properties merging (not only texture) - added support for non-point lights (i.e. global illumination, parallel light rays) - fixed normal vector not being normalized (causing visible models' faces) - fixed specular reflection colour (now dependent only on material's alpha)

--- eel/Shaders/FragmentShader.glsl
+++ eel/Shaders/FragmentShader.glsl
@@ -1,86 +1,142 @@
 #version 330 core
 // Blinn-Phong shading with attenuation, soft cut-off for specular light and using energy conserving model
+
 const float PI = 3.14159265;
+const int MAX_LIGHTS = 4;
+const int MAX_MATERIALS = 4;
+const bool blinn = true;
+
+// Unused
+struct Light {
+	vec3 position;
+	vec3 color;
+	float attenuationConstant;
+	float attenuationLinear;
+	float attenuationQuadratic;
+};
 
 layout(location = 0) out vec4 out_color; // To framebuffer, color location
 
-uniform sampler2D texture0;
-uniform sampler2D texture1;
-
+// From scene manager
+// Globals
 uniform float timePassed;
 uniform vec3 eyePosition;
+// Lights
+//MAX_LIGHTS
+uniform mat3 lights[4];
+uniform bool lightIsPointlight[4];
+uniform int lightCount;
 
-uniform vec3 light0Position;
-uniform vec3 light0Color;
-uniform vec3 light1Position;
-uniform vec3 light1Color;
+// Materials (from model)
+// Properties: ambient, diffusive, specular, shininess
+//MAX_MATERIALS
+uniform sampler2D texture0;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform sampler2D texture3;
+uniform vec4 materialProperties[4];
+uniform vec2 materialSpeeds[4];
+uniform int materialCount;
 
-uniform float materialAmbient;
-uniform float materialDiffusive;
-uniform float materialSpecular;
-uniform float materialShininess;
-
-uniform float attenuationConstant;
-uniform float attenuationLinear;
-uniform float attenuationQuadratic;
-
-// From Vertex shader, preserve names!
+// Vertex properties, from Vertex shader, preserve names!
 in vec3 worldPosition;
 in vec3 worldNormal;
 in vec2 textureCoords;
 in vec4 color;
 
-void calculateLight(in vec3 lightPosition, in vec3 lightColor, inout vec3 diffuseSum, inout vec3 specularSum, in bool blinn) {
+sampler2D getTexture(in int index) {
+	// MAX_MATERIALS
+	switch(index) {
+		case 0:
+			return texture0;
+		case 1:
+			return texture1;
+		case 2:
+			return texture2;
+		case 3:
+			return texture3;
+	}
+}
+
+void calculateMaterial(in sampler2D texturePtr, in vec4 matProps, in vec2 matSpeeds, inout vec4 baseColor, inout vec4 baseProperties) {
+	vec2 texturePosition = matSpeeds * timePassed + textureCoords;
+	vec4 textureColor = texture(texturePtr, texturePosition);
+	float textureAlpha = textureColor.a;
+	textureColor.a = 1.0;
+
+	baseColor = mix(baseColor, textureColor, textureAlpha);
+	baseProperties = mix(baseProperties, matProps, textureAlpha);
+}
+
+void calculateLight(in mat3 light, in bool isPointLight, in float shininess, inout vec3 diffuseSum, inout vec3 specularSum) {
+	vec3 lPos = light[0];
+	vec3 lColor = light[1];
+	float attCons = light[2].x;
+	float attLin = light[2].y;
+	float attQuad = light[2].z;
+	vec3 normal = normalize(worldNormal);
+
 	// Direction TO viewer and light
-	vec3 viewDirection = normalize(eyePosition - worldPosition);
-	vec3 lightDirection = normalize(lightPosition - worldPosition);
-	float lambertian = dot(worldNormal, lightDirection);
+	vec3 lightDirection;
+	if(isPointLight) {
+		// Point light - direction to light source is direction to light source position
+		lightDirection = normalize(lPos - worldPosition);
+	} else {
+		// Global illumination - direction to light source is reverse light emission direction
+		lightDirection = -lPos;
+	}
+	float lambertian = dot(normal, lightDirection);
 	// We're interested in this light source only if angle <= +-90deg
-	float d = distance(lightPosition, worldPosition);
-	float attenuation = 1.0 / (attenuationConstant + d * attenuationLinear + d * d * attenuationQuadratic);
+	float d = distance(lPos, worldPosition);
+	float att = 1.0 / (attCons + d * attLin + d * d * attQuad);
 		
 	if(lambertian > 0.0) {
-		diffuseSum += attenuation * lightColor * materialDiffusive * lambertian;
+		diffuseSum += att * lColor * lambertian;
 	}
 
-	vec3 reflectDirection;
-	float energyCoefficient;
+	vec3 reflectDir;
+	float energyCoeff = 0.0;
 	if(blinn) {
 		// Blinn-Phong
-		energyCoefficient = (8.0 + materialShininess) / (8.0 * PI);
+		energyCoeff = (8.0 + shininess) / (8.0 * PI);
 		// halfway between direction to light and to viewer
-		reflectDirection = normalize(lightDirection + viewDirection);
+		vec3 viewDirection = normalize(eyePosition - worldPosition);
+		reflectDir = normalize(lightDirection + viewDirection);
 	} else {
 		// Phong
-		energyCoefficient = (2.0 + materialShininess) / (2.0 * PI);
+		energyCoeff = (2.0 + shininess) / (2.0 * PI);
 		// Straight reflection
-		reflectDirection = reflect(-lightDirection, worldNormal);
+		reflectDir = reflect(-lightDirection, normal);
 	}
-	float angle = dot(worldNormal, reflectDirection);
+	float angle = dot(normal, reflectDir);
 	if(angle > 0.0) {
-		specularSum += attenuation * lightColor * materialSpecular * energyCoefficient * pow(angle, materialShininess * 4.0);
+		specularSum += lColor * att * energyCoeff * pow(angle, shininess * 4.0);
 	}
 }
 
 void main(void) {
-	// Base color and texture
-	float timeOffset = timePassed * 0.1;
-	vec4 color0 = texture(texture0, vec2(textureCoords.x, textureCoords.y));
-	out_color = mix(color, color0, color0.a);
-	vec4 color1 = texture(texture1, vec2(textureCoords.x, textureCoords.y - timeOffset));
-	float alpha = color1.a;
-	color1.a = 1.0;
-	out_color = mix(out_color, color1, alpha);
+	// Base color and material properties
+	vec4 baseColor = color;
+	vec4 baseProperties = materialProperties[0];
+
+	// Materials
+	for(int i = 0; i < materialCount; i++) {
+		calculateMaterial(getTexture(i), materialProperties[i], materialSpeeds[i], baseColor, baseProperties);
+	}
+	
+	float ambient = baseProperties.x;
+	float diffusive = baseProperties.y;
+	float specular = baseProperties.z;
+	float shininess = baseProperties.w;
 
 	// Lights
-	bool blinn = true;
 	vec3 diffuseSum = vec3(0.0);
 	vec3 specularSum = vec3(0.0);
+	for(int i = 0; i < lightCount; i++) {
+		calculateLight(lights[i], lightIsPointlight[i], shininess, diffuseSum, specularSum);
+	}
 
-	calculateLight(light0Position, light0Color, diffuseSum, specularSum, blinn);
-	calculateLight(light1Position, light1Color, diffuseSum, specularSum, blinn);
-
-	//Fix alpha
-	// max for nice shiny specular reflections
-	out_color.rgb = out_color.rgb * (diffuseSum + materialAmbient) + max(out_color.rgb, 0.05) * specularSum;
+	out_color = baseColor;
+	out_color.rgb *= (diffuseSum * diffusive + vec3(ambient));
+	out_color.rgb += specularSum * baseColor.a * specular;
 }
