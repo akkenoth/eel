a123e091da0cc43003e927208299dc1545e018a2
mjbogusz <mjbogusz+github@gmail.com>
2016-01-26 02:21:18 +0100

    Camera: file not added to previous commit

--- eel/Engine/Camera.cpp
+++ eel/Engine/Camera.cpp
@@ -1,12 +1,10 @@
 #include "Camera.h"
-#include <iostream>
 
 Camera::Camera(glm::vec3 p, glm::vec3 d, glm::vec3 o, float aR, float vFOV, float nC, float fC) {
 	position = p;
 	direction = glm::normalize(d);
 	angleX = glm::acos(-d.z);
 	angleY = glm::atan(d.y, d.x);
-	std::cout << "camera angles: " << angleX <<" | "<< angleY << "\n";
 	orientation = glm::normalize(o);
 	if(aR <= 0.0f) {
 		aR = 1.0f;
@@ -37,31 +35,45 @@ void Camera::setAspectRatio(float aR) {
 }
 
 void Camera::move(float time, bool forward, bool backward, bool left, bool right) {
-	glm::vec4 translation(0.0f);
-	translation.w = 1.0f;
-
-	if(forward && !backward) {
-		translation.y = -1.0f;
-	} else if(!forward && backward) {
-		translation.y = 1.0f;
+	if(!forward && !backward && !left && !right) {
+		return;
 	}
 
+	glm::vec3 translation(0.0f);
+	if(forward && !backward) {
+		translation.z = -1.0f;
+	} else if(!forward && backward) {
+		translation.z = 1.0f;
+	}
 	if(left && !right) {
 		translation.x = -1.0f;
 	} else if(!left && right) {
 		translation.x = 1.0f;
 	}
 
-	glm::mat4 rotation = glm::orientation(direction, orientation);
-	glm::vec4 translationRotated = translation * rotation;
-	position += glm::vec3(translationRotated);
+	position += glm::rotateY(glm::rotateX(translation, angleY), angleX) * time * CAMERA_SPEED;
 }
 
 void Camera::rotate(float time, int x, int y) {
-	// Todo: improve
-	angleX += CAMERA_ROTATION_SPEED * x / aspectRatio;
-	angleY += CAMERA_ROTATION_SPEED * y;
-	direction = glm::rotateY(glm::rotateX(glm::vec3(0.0f, 0.0f, -1.0f), angleX), angleY);
+	if(x == 0 && y == 0) {
+		return;
+	}
+
+	/// Todo: improve by using screen height/width, aspect ratio etc
+	angleX -= CAMERA_ROTATION_SPEED * x;
+	if(angleX < 0.0f) {
+		angleX += 2.0f * M_PI;
+	} else if(angleX >= 2 * M_PI) {
+		angleX -= 2.0f * M_PI;
+	}
+	angleY -= CAMERA_ROTATION_SPEED * y;
+	if(angleY <= -0.5f * M_PI) {
+		angleY = -0.5f * M_PI;
+	} else if(angleY >= 0.5f * M_PI) {
+		angleY = 0.5f * M_PI;
+	}
+
+	direction = glm::rotateY(glm::rotateX(glm::vec3(0.0f, 0.0f, -1.0f), angleY), angleX);
 }
 
 glm::mat4 Camera::getViewMatrix() const {
