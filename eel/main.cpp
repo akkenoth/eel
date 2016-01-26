a164d06860db094e51f97f43ab49969285cefee3
mjbogusz <mjbogusz+github@gmail.com>
2016-01-26 14:28:35 +0100

    main.cpp: added skybox and sun sphere

--- eel/main.cpp
+++ eel/main.cpp
@@ -2,6 +2,7 @@
 
 #include "Engine/Engine.h"
 #include "Models/Sphere.h"
+#include "Models/Skybox.h"
 #include "Libs/glm/detail/type_vec3.hpp"
 
 #ifdef _WIN32
@@ -28,14 +29,24 @@ int main(int argc, char** argv) {
 	engine->getSceneManager()->addLight(0, glm::vec3(0.0f, 5.0f, 0.0f), glm::vec3(1.0), 1.0f, 0.03f, 0.01f);
 	engine->getSceneManager()->addLight(0, glm::vec3(3.0f, -5.0f, 0.0f), glm::vec3(4.0f, 1.0f, 1.0f), 1.0f, 0.03f, 0.005f);
 
+	Skybox* skybox = new Skybox();
+	skybox->create(40.0f);
+	skybox->addMaterial(0, "Resources/Skybox/skybox1.bmp", 1.0f, 0.0f, 0.0f, 0.0f, engine->getTextureLoader());
+	engine->getModelManager()->setModel("skybox", skybox);
+
+	Sphere* sun = new Sphere();
+	sun->create(1.5f, 16, 16, glm::vec4(1.0f, 1.0f, 0.9f, 1.0f));
+	sun->setBaseMaterial(1.0f, 0.0f, 0.0f, 0.0f);
+	sun->setPosition(glm::vec3(9.0f, 20.0f, -12.0f));
+	engine->getModelManager()->setModel("sun", sun);
+
 	Sphere* sphere = new Sphere();
-	sphere->create(1.0, 24, 48);
+	sphere->create(1.0f, 24, 48);
 	sphere->setRotation(glm::vec3(30.0f, 0.0f, 0.0f));
 	sphere->setRotationSpeed(glm::vec3(0.0f, 20.0f, 0.0f));
 	sphere->addMaterial(0, "Resources/Textures/sphere1.bmp", 0.02f, 0.3f, 2.5f, 15.0f, engine->getTextureLoader());
 	sphere->addMaterial(1, "Resources/Textures/sphere_dots.png", 0.02f, 0.8f, 1.0f, 1.0f, engine->getTextureLoader());
 	sphere->addNormalMap("Resources/Textures/fish-normal.bmp", engine->getTextureLoader());
-
 	engine->getModelManager()->setModel("sphere", sphere);
 	
 	engine->run();
